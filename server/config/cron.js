1d716d96b5f4965c298016fce26c6036c9b72f39
japhendywijaya <jap_hendy_wijaya@yahoo.co.id>
2020-01-30 18:43:53 +0700

    jap hendy wijaya

--- server/config/cron.js
+++ server/config/cron.js
@@ -1,12 +1,13 @@
 const cron = require('node-cron')
 const nodemailer = require('nodemailer')
 
+console.log(' \n======================\n JALAN INIH')
+let testAccount = nodemailer.createTestAccount()
+require('dotenv').config()
 
-// let testAccount = nodemailer.createTestAccount()
-
-cron.schedule("* * 10 * * *", function(){
+cron.schedule("0 0 20 * */1 *", function(){
     console.log('TCL \n============\n running cron job' )
-
+    
     let transporter = nodemailer.createTransport({
         service:'Gmail',
         auth:{
@@ -16,17 +17,17 @@ cron.schedule("* * 10 * * *", function(){
     })
 
     const mailOptions = {
-        from: "zenchouu@gmail.com",
-        to: 'bezita.dragonbos@gmail.com',
+        from: "blablabla@blabla.com",
+        to: 'zenchouu@gmail.com',
         subject: 'JALAN NIH CRON',
         text: 'lorem ipsum nblablablablabla ',
-        html: '<b>A Message from Node Cron App</b>'
+        html: '<b>A Message from Node Cron App, minggu depan bulan neraka dimulai. phase 3</b>'
     }
 
     transporter.sendMail(mailOptions, function(error, info){
-        console.log('TCL \n============\n ', info)
+        console.log(`TCL: info`, info)
         if(error){
-            console.log('TCL \n============\n ', error)
+            console.log(`TCL: error`, error)
         }else{
             console.log('TCL \n============\n ', 'Email successfully sent')
         }
