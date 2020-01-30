3a63cd8b61cc60835aee4a19d55a68e197e938d4
zoe-zachary <zoezachary27@gmail.com>
2020-01-30 17:15:17 +0700

    cronjob jalan.

--- server/cronjobs.js
+++ server/cronjobs.js
@@ -1,7 +1,12 @@
 const cron = require('node-cron')
 const app = require('express')()
-const fs = require('fs')
 const nodemailer = require('nodemailer')
+const mongoose = require('mongoose')
+mongoose.connect(`mongodb://localhost:27017/hackoverflow04`, {
+   useCreateIndex: true,
+   useNewUrlParser: true,
+   useUnifiedTopology: true
+})
 
 const Question = require('./models/Question')
 
@@ -10,10 +15,25 @@ Question.find()
    const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
-         zoe: 'zoe.development27@gmail.com',
-         password: 'Development23'
+         user: 'zoe.development27@gmail.com',
+         pass: 'hacktiv8development'
       }
    })
+
+   // console.log('sending first email before starting cronjobs')
+   // let mailOptions = {
+   //    from: 'zoe.development27@gmail.com',
+   //    to: 'zoe.zachary.27@gmail.com',
+   //    subject: 'Your website is growing!',
+   //    text: `Hi Zoe, your website is now having ${questions.length} questions!`
+   // }
+
+   // transporter.sendMail(mailOptions, (err, results) => {
+   //    if(err) throw err
+   //    else {
+   //       console.log('Email sent!', results)
+   //    }
+   // })
 
    cron.schedule("0 0 * * 1", function() {
       const mailOptions = {
@@ -25,9 +45,12 @@ Question.find()
 
       transporter.sendMail(mailOptions, (err, results) => {
          if(err) throw err
+         else {
+            console.log('Email sent!', results)
+         }
       })
    })
 })
 .catch(error => console.log(error))
 
-app.listen(3120)
+app.listen(3120, () => console.log(`Cronjob is running on port 3120`))
