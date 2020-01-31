216ae1fc4f70af43781e5e564b6aa1f5778b6b2a
muhafaa <muhafa99@gmail.com>
2020-01-31 11:16:16 +0700

    nambahin authorize buat answer dan ubah urutan prioritas untuk route answer biar user yg belum login bisa dapet answer

--- server/routes/answer.js
+++ server/routes/answer.js
@@ -4,20 +4,22 @@ const ControllerAnswer = require('../controllers/answer')
 
 const authenticate = require('../middlewares/authenticate')
 
+answerRouter.get('/question/:id', ControllerAnswer.find)
+
 answerRouter.use(authenticate)
 
 answerRouter.post('/', ControllerAnswer.create)
-
-answerRouter.get('/question/:id', ControllerAnswer.find)
 
 answerRouter.get('/answer/:id', ControllerAnswer.findOne)
 
 answerRouter.get('/lists', ControllerAnswer.findOwn)
 
-answerRouter.put('/update/:id', ControllerAnswer.update)
-
 answerRouter.post('/upvote/:id', ControllerAnswer.upvote)
 
 answerRouter.post('/downvote/:id', ControllerAnswer.downvote)
+
+const authorize = require('../middlewares/authorize-answer')
+
+answerRouter.put('/update/:id', authorize, ControllerAnswer.update)
 
 module.exports = answerRouter
